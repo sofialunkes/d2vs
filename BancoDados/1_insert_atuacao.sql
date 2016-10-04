0273c438c02cb4070e60b0ce1fc4a388cd5323f5
Sofia <sofia.big@live.com>
2016-10-04 01:35:40 -0300

    atualização do banco

---
+++ BancoDados/1_insert_atuacao.sql
@@ -0,0 +1,4 @@
+INSERT INTO atu_atuacao (atu_area) values
+('Tecnologia da informação'),
+('Educação'),
+('Gastronomia');
---
+++ BancoDados/2_insert_profissao.sql
@@ -0,0 +1,4 @@
+INSERT INTO pro_profissao (pro_nome, atu_codigo) values 
+('Professor de inglês',2),
+('Garçom',3),
+('Programador Web',1);
