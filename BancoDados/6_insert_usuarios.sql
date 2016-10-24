7aed668898b0e529a97f5ba1be7efc954f30f245
BianchiD <codv.ov2@gmail.com>
2016-10-24 17:15:43 -0200

    Relatório técnico atualizado

--- BancoDados/6_insert_usuarios.sql
+++ BancoDados/6_insert_usuarios.sql
@@ -1,5 +1,5 @@
-INSERT INTO `d2vs`.`usu_usuario` (`USU_NOME`, `USU_SOBRENOME`, `USU_EMAIL`, `USU_SENHA`, `USU_SEXO`, `USU_CPF`, `USU_RG`, `USU_DT_NASCIMENTO`, `USU_CELULAR`, `USU_TELEFONE`, `USU_BAIRRO`, `USU_ENDERECO`, `USU_CEP`, `USU_COMPLEMENTO`, `USU_ATIVO`, `USU_PAGA_PREMIUM`, `CID_CODIGO`, `TUS_CODIGO`) 
-VALUES ('Autonomo', 'Autonomo','aut@aut.com', 'PJkJr+wlNU1VHa4hWQuybjjVPyFzuNPcPu5MBH56scHri4UQPjvnumE7MbtcnDYhTcnxSkL9ei/bhIVrylxEwg==', 'm', '1', '1', '1990-01-01', '1', '1', '1', '1', '1', '1', 's', 'n', '1', '1');
+INSERT INTO `d2vs`.`usu_usuario` (`USU_NOME`, `USU_SOBRENOME`, `USU_EMAIL`, `USU_SENHA`, `USU_SEXO`, `USU_CPF`, `USU_DT_NASCIMENTO`, `USU_CELULAR`, `USU_TELEFONE`, `USU_BAIRRO`, `USU_ENDERECO`, `USU_CEP`, `USU_COMPLEMENTO`, `USU_ATIVO`, `USU_PAGA_PREMIUM`, `CID_CODIGO`, `TUS_CODIGO`) 
+VALUES ('Autonomo', 'Autonomo','aut@aut.com', 'PJkJr+wlNU1VHa4hWQuybjjVPyFzuNPcPu5MBH56scHri4UQPjvnumE7MbtcnDYhTcnxSkL9ei/bhIVrylxEwg==', 'm', '1', '1990-01-01', '1', '1', '1', '1', '1', '1', 's', 'n', '1', '1');
 
 INSERT INTO `d2vs`.`usu_usuario` (`USU_NOME`, `USU_SOBRENOME`, `USU_EMAIL`, `USU_SENHA`, `CID_CODIGO`, `TUS_CODIGO`) 
 VALUES ('Cliente', 'Cliente', 'cli@cli.com', 'PJkJr+wlNU1VHa4hWQuybjjVPyFzuNPcPu5MBH56scHri4UQPjvnumE7MbtcnDYhTcnxSkL9ei/bhIVrylxEwg==', '1', '2');
