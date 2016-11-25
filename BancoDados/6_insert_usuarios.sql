cd740b2af5c1762ba2998f50cb06b0d5d6a90427
SHIMURA\vitor <vitor.shimura@hotmail.com>
2016-11-24 22:52:07 -0200

    Novos Inserts

--- BancoDados/6_insert_usuarios.sql
+++ BancoDados/6_insert_usuarios.sql
@@ -9,3 +9,24 @@ VALUES ('Administrador', 'Administrador', 'adm@adm.com', 'PJkJr+wlNU1VHa4hWQuybj
 
 INSERT INTO `d2vs`.`usu_usuario` (`USU_NOME`, `USU_SOBRENOME`, `USU_EMAIL`, `USU_SENHA`, `USU_SEXO`, `USU_CPF`, `USU_DT_NASCIMENTO`, `USU_CELULAR`, `USU_TELEFONE`, `USU_BAIRRO`, `USU_ENDERECO`, `USU_CEP`, `USU_COMPLEMENTO`, `USU_ATIVO`, `USU_PAGA_PREMIUM`, `CID_CODIGO`, `TUS_CODIGO`) 
 VALUES ('Autonomo2', 'Autonomo2','aut2@aut2.com', 'PJkJr+wlNU1VHa4hWQuybjjVPyFzuNPcPu5MBH56scHri4UQPjvnumE7MbtcnDYhTcnxSkL9ei/bhIVrylxEwg==', 'm', '4', '1990-01-01', '1', '1', '1', '1', '1', '1', 's', 'n', '1', '1');
+
+-- Clientes 
+
+INSERT INTO `d2vs`.`usu_usuario` (`USU_NOME`, `USU_SOBRENOME`, `USU_EMAIL`, `USU_SENHA`, `USU_SEXO`, `USU_CPF`, `USU_DT_NASCIMENTO`, `USU_CELULAR`, `USU_TELEFONE`, `USU_BAIRRO`, `USU_ENDERECO`, `USU_CEP`, `USU_COMPLEMENTO`, `USU_ATIVO`, `USU_NUM_CASA`,`USU_PAGA_PREMIUM`, `CID_CODIGO`, `TUS_CODIGO`) 
+VALUES ('João', 'da Silva','joao@joao.com', 'PJkJr+wlNU1VHa4hWQuybjjVPyFzuNPcPu5MBH56scHri4UQPjvnumE7MbtcnDYhTcnxSkL9ei/bhIVrylxEwg==', 'm', '12345678901', '1990-01-01', '012981470555', '01231253534', 'Jardim do Vale', 'Rua dos Joões', '12500000', 'Casa', 's', '52', 'n', '4916', '2');
+
+INSERT INTO `d2vs`.`usu_usuario` (`USU_NOME`, `USU_SOBRENOME`, `USU_EMAIL`, `USU_SENHA`, `USU_SEXO`, `USU_CPF`, `USU_DT_NASCIMENTO`, `USU_CELULAR`, `USU_TELEFONE`, `USU_BAIRRO`, `USU_ENDERECO`, `USU_CEP`, `USU_COMPLEMENTO`, `USU_ATIVO`, `USU_NUM_CASA`, `USU_PAGA_PREMIUM`, `CID_CODIGO`, `TUS_CODIGO`) 
+VALUES ('Mariana', 'dos Reis','mariana@mariana.com', 'PJkJr+wlNU1VHa4hWQuybjjVPyFzuNPcPu5MBH56scHri4UQPjvnumE7MbtcnDYhTcnxSkL9ei/bhIVrylxEwg==', 'f', '12345678902', '1995-01-01', '012981470557', '01231253734', 'Nova Lorena', 'Rua das Marianas', '12563000', 'Casa', 's', '43', 'n', '5014', '2');
+
+-- Autonomos
+
+INSERT INTO `d2vs`.`usu_usuario` (`USU_NOME`, `USU_SOBRENOME`, `USU_EMAIL`, `USU_SENHA`, `USU_SEXO`, `USU_CPF`, `USU_DT_NASCIMENTO`, `USU_CELULAR`, `USU_TELEFONE`, `USU_BAIRRO`, `USU_ENDERECO`, `USU_CEP`, `USU_COMPLEMENTO`, `USU_ATIVO`, `USU_NUM_CASA`, `USU_PAGA_PREMIUM`, `CID_CODIGO`, `TUS_CODIGO`) 
+VALUES ('Maria', 'Martins','maria@maria.com', 'PJkJr+wlNU1VHa4hWQuybjjVPyFzuNPcPu5MBH56scHri4UQPjvnumE7MbtcnDYhTcnxSkL9ei/bhIVrylxEwg==', 'f', '12345678904', '1985-01-01', '012981470547', '01231223734', 'Velha Lorena', 'Rua das Marias', '12563054', 'Apartamento', 's', '35A', 'n', '5014', '1');
+
+
+INSERT INTO `d2vs`.`usu_usuario` (`USU_NOME`, `USU_SOBRENOME`, `USU_EMAIL`, `USU_SENHA`, `USU_SEXO`, `USU_CPF`, `USU_DT_NASCIMENTO`, `USU_CELULAR`, `USU_TELEFONE`, `USU_BAIRRO`, `USU_ENDERECO`, `USU_CEP`, `USU_COMPLEMENTO`, `USU_ATIVO`, `USU_NUM_CASA`, `USU_PAGA_PREMIUM`, `CID_CODIGO`, `TUS_CODIGO`) 
+VALUES ('Albertino', 'Sampaio','albertino@albertino.com', 'PJkJr+wlNU1VHa4hWQuybjjVPyFzuNPcPu5MBH56scHri4UQPjvnumE7MbtcnDYhTcnxSkL9ei/bhIVrylxEwg==', 'm', '12345678905', '1975-01-01', '012999470547', '01231223763', 'Nova Guará', 'Rua dos Alberts', '12843054', 'Casa', 's', '846', 'n', '4916', '1');
+
+INSERT INTO `d2vs`.`usu_usuario` (`USU_NOME`, `USU_SOBRENOME`, `USU_EMAIL`, `USU_SENHA`, `USU_SEXO`, `USU_CPF`, `USU_DT_NASCIMENTO`, `USU_CELULAR`, `USU_TELEFONE`, `USU_BAIRRO`, `USU_ENDERECO`, `USU_CEP`, `USU_COMPLEMENTO`, `USU_ATIVO`, `USU_NUM_CASA`, `USU_PAGA_PREMIUM`, `CID_CODIGO`, `TUS_CODIGO`) 
+VALUES ('Felipe', 'Martins','felipe@felipe.com', 'PJkJr+wlNU1VHa4hWQuybjjVPyFzuNPcPu5MBH56scHri4UQPjvnumE7MbtcnDYhTcnxSkL9ei/bhIVrylxEwg==', 'm', '12345678906', '1995-01-01', '012981477633', '01236723734', 'Centro', 'Rua dos Felipes', '12563056', 'Casa', 's', '363', 'n', '4916', '1');
+
---
+++ BancoDados/7_insert_usuario_profissional.sql
@@ -0,0 +1,9 @@
+INSERT INTO `d2vs`.`USP_USUARIO_PROFISSAO` (`PRO_CODIGO`, `USU_CODIGO`, `USP_DESCRICAO`) 
+VALUES (1, 7, "Professora de Inglês por mais de 5 anos, experiência internacional");
+
+INSERT INTO `d2vs`.`USP_USUARIO_PROFISSAO` (`PRO_CODIGO`, `USU_CODIGO`, `USP_DESCRICAO`) 
+VALUES (2, 9, "Garçom experiente, trabalho em casamentos, formaturas e festas em geral");
+
+INSERT INTO `d2vs`.`USP_USUARIO_PROFISSAO` (`PRO_CODIGO`, `USU_CODIGO`, `USP_DESCRICAO`) 
+VALUES (3, 8, "Programador web, com habilidades em .Net, Js, C#");
+
